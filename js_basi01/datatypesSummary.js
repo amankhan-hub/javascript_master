a5cfa420729343cc3200c4d11215014da7773c05
Aman Khan <ak038773@gmail.com>
2024-09-25 11:13:16 +0000

    premitive and non

---
+++ js_basi01/datatypesSummary.js
@@ -0,0 +1,37 @@
+ //primitive
+
+ // 7 types 
+ // String
+ // Numbers
+ // Boolean
+ // Null
+ // Undefined
+ // Symbol
+ // BigInt
+                    //********we does not tell the java script what  will be the type of variable so dynamic type */
+ // Reference or Non Primitive
+ // Arrays
+ // Objects
+ // Functions
+
+ //Array
+ const heros=['aman','bilal','nabeel']
+ console.log(heros);
+ 
+
+ //Object
+ let data={
+    name:'aman'
+    ,city:'lahore'
+ }
+ console.log(data);
+ 
+
+ //function
+
+ let myfunction=function() {                //function object
+    console.log("hello");
+    
+ };
+myfunction();
+ 
