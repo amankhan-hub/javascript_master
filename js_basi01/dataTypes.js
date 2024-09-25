6d797932a046c7591242e098ac1e0a60a8472ba4
Aman Khan <ak038773@gmail.com>
2024-09-25 09:21:58 +0000

    dataTypes

---
+++ js_basi01/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all js code in newer version
+
+
+// alert(3+3) //we are using node js not browser   
+
+console.log(3+3)    /* The Code Should be rediable it can work like 
+                    console.log(3+3);console.log("Aman") but not redable*/
+console.log("Aman")
+
+let name="AMAN"
+let age=18
+let isLoggedIn=false
+
+/*
+number=>2 to power 53
+bigint if the number is too big
+string=>""
+boolean=>true/false
+null=> standalone value   like temperature
+undefined=>no value given
+symbol=> unique
+object
+*/
+
+console.log(typeof age);      //its a number
+
+console.log(typeof null);     //its type is object
+
+console.log(typeof undefined);  //its type is undefined
+
--- js_basi01/variables.js
+++ js_basi01/variables.js
@@ -1,1 +1,15 @@
-console.log("aman")
+const accountId=1144553   //cant be changed 
+let accountEmail="ak038773@gmail.com"                  // it has scope only can be used within the scope
+var accountPassword="12345"                            //scope issue it can be accesed and can change as it has no scope 
+accountCity="lahore"                                   //memory can be reserved and the variable can be defined
+let accoutState;                                       //undefined
+
+
+//accountId="67890"  not allowed
+
+accountEmail="ak03877@gmail.com"
+accountPassword="78906"
+accountCity="Karachi"
+
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutState]);  //the other way to print the data
