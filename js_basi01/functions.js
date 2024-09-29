03d785f8fa14b2b56d3ff361dba5d4f4d96892e2
Aman Khan <ak038773@gmail.com>
2024-09-29 15:23:35 +0000

    functions

---
+++ js_basi01/functions.js
@@ -0,0 +1,29 @@
+// function saymyname()
+// {
+//     console.log("hello there")
+// }
+
+// saymyname    //this is the reference
+
+// saymyname()  //execution with the braces
+
+// function addnnumber(num1,num2)  //parameter
+// {
+//     return num1+num2    //after return any other line would not be printed
+// }
+
+// let resulty=(addnnumber(1,5))    ///arguments
+// console.log(resulty)
+
+// function name(result)
+// {
+//     if(result===resulty)
+//      return result
+//     else
+//     console.log("havimg issue")
+//     return
+// }
+// let result1=name(6)
+// console.log(result1)
+
+
