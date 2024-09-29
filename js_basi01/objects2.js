daf2ab80f67f82352f1f16de28707d6b2fd6bb00
Aman Khan <ak038773@gmail.com>
2024-09-29 10:48:04 +0000

    objects done

---
+++ js_basi01/objects2.js
@@ -0,0 +1,35 @@
+//const tinderuser=new Object()  //singleton
+const tinderuser={
+    name:"aman",
+    age:18,
+    address:{
+        city:"lahore",
+        society:"askari 10",
+        parmannetaddress:{
+            pcity:"rawalpindi"
+        }
+    }
+}
+//console.log(tinderuser["address"]["parmannetaddress"]["pcity"])
+
+let obj1={
+    a:"1",
+    b:"2",
+    c:"3"
+}
+let obj2={
+    d:"4",
+    e:"5",
+    f:"6"
+}
+let obj3={
+    g:"7",
+    e:"8",
+    h:"9"
+}
+let last=Object.assign({},obj1,obj2,obj3)
+console.log(last)
+
+
+let result={...obj1,...obj2,...obj3}
+console.log(result);
