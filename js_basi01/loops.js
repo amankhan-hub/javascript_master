38ac3f696370c11c3683965c49ca8171d1bf5509
Aman Khan <ak038773@gmail.com>
2024-10-05 17:54:17 +0000

    loops and other completed

---
+++ js_basi01/loops.js
@@ -0,0 +1,67 @@
+// let arr1=["javascript","c++","C#"]
+// const values=arr1.forEach((item)=>{
+//      console.log(item)
+//     //return item nothing xhanged
+
+// })
+// console.log(values)
+
+
+// const myNums=[1,2,3,4,5,6,7,8,9,10]
+// let numbers=myNums.filter((num)=> {if(num>=4) return num} )
+//      console.log(numbers)
+
+
+// let a1=[1,2,3,4,5,6,7]
+// let result=a1.filter((item)=>item>6)
+// console.log(result)
+
+// let a2=[]
+// let a1=[1,2,3,4,5,6,7]
+// let result=a1.forEach((item)=>{
+//      if(item>6)
+//     a2.push(item) 
+// })
+// console.log(a2)
+// let arr2=[]
+
+// let arr1=[{name:"aman",place:"pakistan"}
+//      ,{name:"alina",place:"india"},
+//      {name:"sami",place:"enngland"}
+// ]
+// let result=arr1.forEach((item)=>{if(item.name==="aman"|| item.name==="alina")
+//      arr2.push(item)
+// })
+// console.log(arr2)
+
+
+// let mynum=[1,2,3,4,5,6]
+// let thenums=mynum.reduce((acc,curval)=>{
+//      console.log(`the value of acc is ${acc} and the cur value is ${curval}`)
+//      return acc+curval
+// })
+// console.log(thenums)
+
+
+// let mynums=[1,2,3,4,5,6]
+// let any=mynums.reduce((acc,curval)=>acc+curval,5)
+// console.log(any)
+
+// let shoppingcart=[{
+//      itemname:"js"
+//      ,price: 500
+// },{
+//      itemname: "mad"
+//      ,price:1000
+// },
+// {
+//      itemname: "web"
+//      ,price:2000
+// },{
+//      itemname: "react"
+//      ,price:4000
+// },]
+
+// let total=shoppingcart.reduce((acc,curval)=>{return acc+curval.price},0)
+// console.log(total)    //for each and map can also be used
+
